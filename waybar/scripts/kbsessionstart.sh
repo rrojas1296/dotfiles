c95a43e1411a394dd92d12673a163c0b850b9a84
Diego Rojas <rrojas1296@gmail.com>
2026-02-07 11:06:29 -0500

    master: Edited whole config

--- waybar/scripts/kbsessionstart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Iniciar gnome-keyring daemon (claves, ssh, secrets)
-eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
-export SSH_AUTH_SOCK
-export GNOME_KEYRING_CONTROL
-export DBUS_SESSION_BUS_ADDRESS
