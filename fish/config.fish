780d8d6cd6574a4f3b6c57c23ea1f7c89a9be7a4
Diego Raul Rojas <rrojas1296@gmail.com>
2026-05-21 21:37:45 -0500

    master: Edited fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,5 @@
 set -g fish_greeting
 
-# ----------------
-# Git prompt setup
-# ----------------
 set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showupstream informative
@@ -11,38 +8,27 @@ set -g __fish_git_prompt_color_dirty red
 set -g __fish_git_prompt_color_staged green
 set -g __fish_git_prompt_color_upstream cyan
 
-# ----------------
-# Custom Fish Prompt
-# ----------------
 function fish_prompt
-    # Arch Linux logo
     set_color blue --bold
     echo -n " "
 
-    # Current directory
     set_color cyan
     echo -n (prompt_pwd) " "
 
-    # Git repository status
     set_color blue
     __fish_git_prompt " %s "
 
-    # Node.js version (GREEN)
     if type -q node
         set_color green --bold
         echo -n "⬢ "(node -v)" "
     end
 
-    # Prompt symbol
     set_color blue --bold
     echo -n "❯ "
 
     set_color normal
 end
 
-# ----------------
-# Right prompt (optional)
-# ----------------
 function fish_right_prompt
     set_color brblack
     date "+%H:%M"
@@ -54,22 +40,11 @@ alias z="zellij"
 alias ll="ls -la"
 
 if status is-interactive
-    and not set -q TMUX  # opcional, si no quieres en tmux
-    and test -z "$NVIM"  # opcional, para no mostrar en neovim
+    and not set -q TMUX
+    and test -z "$NVIM"
     fastfetch 
 end
 
 export PATH="$HOME/.config/waybar/scripts:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export TERMINAL="ghostty"
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# Gnome keyring
-set -x GTK_USE_PORTAL 1
-set -x XDG_CURRENT_DESKTOP Hyprland
-
-# opencode
-fish_add_path /home/oasis/.opencode/bin
